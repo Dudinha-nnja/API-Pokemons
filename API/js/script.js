f673a1086085aa076008c74d64ae18f327b7481a
Dudinha-nnja <eduardarod04@gmail.com>
2025-06-16 17:43:49 -0300

    v1

---
+++ API/js/script.js
@@ -0,0 +1,24 @@
+api = "https://pokeapi.co/api/v2/";
+
+endpoint = "pokemon?limit=1000";
+
+consulta = api + endpoint;
+//lista pokemons 
+fetch(consulta).then((resposta) => resposta.json()).then((dados) => {
+    //console.log(dados);
+    dados.results.map((infoPokemon) => {
+        console.log(infoPokemon);
+        //listar informações de cada pokemon
+        fetch(infoPokemon.url).then((resposta) => resposta.json()).then((dadosDeCadaPoke) => {
+
+            console.log(dadosDeCadaPoke);
+            document.querySelector("#pokemons").innerHTML += `
+            <div class="pokemon card p-4 d-flex align-itens-cente">
+            <img style="width:300px;" src="${dadosDeCadaPoke.sprites.front_default}"
+            <h4>${dadosDeCadaPoke.name}</h4>
+            </div>
+               `;
+
+        });
+    });
+});
